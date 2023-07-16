00a71b03bee5a2a1c808ceab569b79e92c49d8a5
anisoara1 <122683089+anisoara1@users.noreply.github.com>
2023-07-16 19:05:49 +0000

    Deploying to gh-pages from @ anisoara1/BarberShop_page@25625b29360f1951deb69d116c10d9bac7265f2b 🚀

---
+++ index.1935de4b.js
@@ -0,0 +1,2 @@
+var e=1;!function(l){var s,a=document.getElementsByClassName("slide"),t=document.getElementsByClassName("slider__item");for(l>a.length&&(e=1),l<1&&(e=a.length),s=0;s<a.length;s+=1)a[s].style.display="none";for(s=0;s<t.length;s+=1)t[s].className=t[s].className.replace(" active","");a[e-1].style.display="block",t[e-1].className+=" active"}(1);
+//# sourceMappingURL=index.1935de4b.js.map
